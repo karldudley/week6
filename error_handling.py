5423c0480515c7f5d6ab52de0e3d919d79363d25
karl dudley <karldudley@gmail.com>
2023-01-14 08:22:32 +0000

    changes

--- error_handling.py
+++ error_handling.py
@@ -10,11 +10,16 @@ file = None
 try:
     file = open("input.txt", 'r')
     print("The file was found")
-except:
+except FileNotFoundError as error:
     print("The file was not found")
+    print(error)
 finally:
     if file is not None:
         file.close()
         print("The file was closed")
     else:
         print("No file to close")
+
+num = int(input("Please enter a value greater than 10: "))
+if num < 10:
+    raise Exception('num was less than 10. The value of num was: {}'.format(num))
